35ca60dfe20a46ce0e47a80d43fc2e353db8e5f2
dnqthang <75325436+Sumels010901@users.noreply.github.com>
2022-05-14 13:41:46 +0700

    thêm activity home để demo đăng nhập *anh chị em lưu ý là trang có hoạt động thì thêm activity chứ đừng thêm file xml thuần nhé chạy không được đâu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtUsername, edtPassword;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtUsername = findViewById(R.id.txtUsername);
+        edtPassword = findViewById(R.id.txtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String password = edtPassword.getText().toString();
+                String username = edtUsername.getText().toString();
+                if(username.equals("admin") && password.equals("admin")) {
+                    Toast.makeText(MainActivity.this, "Login success!", Toast.LENGTH_SHORT).show();
+                    switchActivities();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, MainManageActivity.class);
+        startActivity(switchActivityIntent);
+    }
+
 }
