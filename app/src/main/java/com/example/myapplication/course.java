b72e2713cc58be2f90d43389c3abf751b6cde8ea
shido <shidogiang@gmail.com>
2022-05-15 12:07:41 +0700

    fix combobox cua course

--- app/src/main/java/com/example/myapplication/course.java
+++ app/src/main/java/com/example/myapplication/course.java
@@ -4,10 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class course extends AppCompatActivity {
     Button btnCourseBack;
+    String[] ngay = {"thu 2", "thu 3","thu 4","thu 5","thu 6","thu 7"};
+    String[] tiet = {"tiet 1", "tiet 2", "tiet 3", "tiet 4", "tiet 5", "tiet 6"
+            , "tiet 7", "tiet 8", "tiet 9", "tiet 10", "tiet 11"};
+    Spinner j_spinner; //combobox của thứ
+    Spinner i_spinner; //combobox của tiết
+    String TKB_MH1 = "";
+    String TKB_MH2 = "";
+    String TKB_MH = ""; // Chuỗi TKB_MH để cho vào db nè
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +28,44 @@ public class course extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+        j_spinner = findViewById(R.id.spinnerDay);
+
+        i_spinner = findViewById(R.id.spinnerTietHoc);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ngay);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tiet);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        j_spinner.setAdapter(adapter);
+        j_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View view, int arg2, long arg3) {
+                TKB_MH1 = j_spinner.getSelectedItem().toString() + " ";
+                TKB_MH2 =  i_spinner.getSelectedItem().toString();
+                TKB_MH = TKB_MH1 + TKB_MH2;
+                //j_spinner_selected.setText(j_spinner.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+        i_spinner.setAdapter(adapter2);
+        i_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View view, int arg2, long arg3) {
+                TKB_MH1 = j_spinner.getSelectedItem().toString() + " ";
+                TKB_MH2 = i_spinner.getSelectedItem().toString();
+                TKB_MH = TKB_MH1 + TKB_MH2;
+                //i_spinner_selected.setText(i_spinner.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
             }
         });
     }
--- app/src/main/java/com/example/myapplication/student.java
+++ app/src/main/java/com/example/myapplication/student.java
@@ -2,12 +2,18 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class student extends AppCompatActivity {
     Button btnStudentBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,4 +26,5 @@ public class student extends AppCompatActivity {
             }
         });
     }
+
 }
